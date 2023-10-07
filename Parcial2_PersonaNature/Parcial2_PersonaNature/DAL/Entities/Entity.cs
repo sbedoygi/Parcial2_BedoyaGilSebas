b04c2d21c97ea461ed10fc44a1fa0a47e06e9d96
SEBASTIAN BEDOYA GIL <sebastianbedoya151125@correo.itm.edu.co>
2023-10-07 10:46:52 -0500

    Creating Entity Property for Microsoft.EntityFrameworkCore Database Creation

---
+++ Parcial2_PersonaNature/Parcial2_PersonaNature/DAL/Entities/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Parcial2_PersonaNature.DAL.Entities
+{
+    public class Entity
+    {
+        #region
+        [Key]
+        public virtual Guid id { get; set; }
+        public virtual DateTime? CreatedDate { get; set; }
+        public virtual DateTime? ModifiedDate { get; set; }
+        #endregion
+
+    }
+}
