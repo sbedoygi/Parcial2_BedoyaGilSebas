572f9b53b7793dc89ebe738ede920020f03e0add
SEBASTIAN BEDOYA GIL <sebastianbedoya151125@correo.itm.edu.co>
2023-10-07 10:59:23 -0500

    Created a class 'NaturalPerson' inheriting from 'Entity'

---
+++ Parcial2_PersonaNature/Parcial2_PersonaNature/DAL/Entities/NaturalPerson.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Parcial2_PersonaNature.DAL.Entities
+{
+    public class NaturalPerson : Entity
+
+
+    {
+        #region
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Display(Name = "Nombre Completo")]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Display(Name = "Ingrese el Año de Nacimiento")]
+        public int BirthYear { get; set; }
+
+        
+        public int Age { get; set; }
+
+
+        #endregion
+
+    }
+}
