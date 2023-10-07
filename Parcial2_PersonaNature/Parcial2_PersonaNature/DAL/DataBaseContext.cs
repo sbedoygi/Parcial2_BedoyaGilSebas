4ae12ce3d6c4de83329f1e2fca30c02c0e5df4f0
SEBASTIAN BEDOYA GIL <sebastianbedoya151125@correo.itm.edu.co>
2023-10-07 11:22:29 -0500

    Created class DataBaseContext with  FrameworkCore in mvc

---
+++ Parcial2_PersonaNature/Parcial2_PersonaNature/DAL/DataBaseContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Parcial2_PersonaNature.DAL.Entities;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace Parcial2_PersonaNature.DAL
+{
+    public class DataBaseContext : DbContext
+    {
+
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+        }
+        public DbSet<NaturalPerson> NaturalPerson { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<NaturalPerson>().HasIndex(NaturalPerson => NaturalPerson.FullName).IsUnique();
+        }
+
+
+    }
+}
